c429ffa060f82ae40d36cfe0bfbe011238158d22
S-Man129 <adebayosulayman129@gmail.com>
2025-04-17 14:39:01 +0100

    Update: Reset the game after a player Wins

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,67 +1,86 @@
 'use strict';
 
-let player0El = document.querySelector('.player--0');
-let player1El = document.querySelector('.player--1');
-let score0 = document.getElementById('score--0');
-score0.textContent = 0;
+let score0El = document.getElementById('score--0');
+let score1El = document.getElementById('score--1');
+// score0El.textContent = 0;
+// score1El.textContent = 0;
 
-let score1 = document.getElementById('score--1');
-score1.textContent = 0;
-
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+let player0EL = document.querySelector('.player--0');
+let player1EL = document.querySelector('.player--1');
 
 let currentScore0 = document.getElementById('current--0');
 let currentScore1 = document.getElementById('current--1');
+
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+
 let diceEl = document.querySelector('.dice');
 diceEl.classList.add('hidden');
 
-let score = [0, 0];
-let currentScore = 0;
-let currentPlayer = 0;
+let score, currentScore, currentPlayer, gamePlaying;
 
-function switchPlayer() {
+const resetGame = () => {
+  score = [0, 0];
   currentScore = 0;
+  currentPlayer = 0;
+  gamePlaying = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0.textContent = 0;
+  currentScore1.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0EL.classList.remove('player--winner');
+  player1EL.classList.remove('player--winner');
+  player0EL.classList.add('player--active');
+  player1EL.classList.remove('player--active');
+};
+resetGame();
+
+const switchPlayer = () => {
   document.getElementById(`current--${currentPlayer}`).textContent = 0;
+  currentScore = 0;
   currentPlayer = currentPlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-}
+  player0EL.classList.toggle('player--active');
+  player1EL.classList.toggle('player--active');
+};
 
-// Roll dice function
 btnRoll.addEventListener('click', function () {
-  diceEl.classList.remove('hidden');
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  diceEl.src = `dice-${dice}.png`;
+  if (gamePlaying) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // console.log(dice);
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // Add dice to current score
-
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${currentPlayer}`).textContent =
-      currentScore;
-  } else {
-    //switch player
-    switchPlayer();
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${currentPlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  score[currentPlayer] += currentScore;
-  document.getElementById(`score--${currentPlayer}`).textContent =
-    score[currentPlayer];
-  //   switchPlayer();
+  if (gamePlaying) {
+    score[currentPlayer] += currentScore;
+    document.getElementById(`score--${currentPlayer}`).textContent =
+      score[currentPlayer];
 
-  if (score[currentPlayer] >= 20) {
-    // currentPlayer is the winner
-    document
-      .querySelector(`.player--${currentPlayer}`)
-      .classList.add('player--winner');
-    document
-      .querySelector(`.player--${currentPlayer}`)
-      .classList.remove('player--active');
-  } else {
-    switchPlayer();
+    if (score[currentPlayer] >= 100) {
+      // Current player wins the game
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.add('player--winner');
+      diceEl.classList.add('hidden');
+      gamePlaying = false;
+      // btnRoll.setAttribute('disabled', 'disabled');
+      // btnHold.setAttribute('disabled', 'disabled');
+    } else {
+      switchPlayer();
+    }
   }
 });
+
+btnNew.addEventListener('click', resetGame);
