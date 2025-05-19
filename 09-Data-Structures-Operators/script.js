adfc1e435aeb3f8203f99d499c76f83bec5e64b4
S-Man129 <adebayosulayman129@gmail.com>
2025-05-19 15:37:09 +0100

    Update: Completed module on Data structures and Modern operators and String

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -93,7 +93,7 @@ const restaurant = {
 ///////////////////////////////
 // String Method Practice
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384782;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fa093766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const getCode = from => from.slice(0, 3).toUpperCase();
 for (const message of flights.split('+')) {
